38c0453b606dcf4aefb491cb1092f8c939c0eec3
root <root@agori>
2023-08-05 11:43:58 +0500

    in

---
+++ src/components/Product.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function Product(props: any) {
+  return (
+    <div className='border py-2 px-4 rouned flex flex-col items-center mb-2'
+    >
+      Абра кадабра
+    </div>
+  )
+}
